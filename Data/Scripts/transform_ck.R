89c103aa2182b4f70090c3db9e6aae0578535c81
ilyakats <ikats@yahoo.com>
2017-05-13 18:15:57 -0400

    Report

--- Data/Scripts/transform_ck.R
+++ Data/Scripts/transform_ck.R
@@ -4,52 +4,93 @@ library(dplyr)
 library(stringr)
 
 # Load data from CSV into data frame
-bloom <- read.csv2("https://raw.githubusercontent.com/parastyle/DATA-607---Final-Project-/master/Data/bloomingdales-scraped.csv")
+ck <- read.csv2("https://raw.githubusercontent.com/parastyle/DATA-607---Final-Project-/master/Data/ckboxerbriefsResults.csv")
 
 ## ---------------- DATA CLEANUP  ----------------
 
 # Transpose the table
-bloom <- bloom %>%
-  gather(product, value, 2:ncol(bloom)) %>%
+ck <- ck %>%
+  gather(product, value, 2:ncol(ck)) %>%
   spread(X, value)
 
 # Adjust column names
-colnames(bloom) <- c("product.id", "type", "name", "color", "material", "price.original", "price.sale", "reviews", "source")
+colnames(ck) <- c("product.id", "type", "name", "color", "material", "price.original", "price.sale", "reviews", "source")
 
 # Clean up 'product id'
-bloom$product.id <- as.numeric(str_replace(bloom$product.id, "V", ""))
+ck$product.id <- as.numeric(str_replace(ck$product.id, "V", ""))
+ck$product.id <- ck$product.id + 1000
 
 # Clean up 'reviews'
-bloom$reviews <- str_trim(bloom$reviews)
+ck$reviews <- str_trim(ck$reviews)
 
 # Convert 'color' and 'material' to list
-bloom$color <- str_to_lower(str_replace_all(bloom$color, "(c\\(|\\)|\"| )", ""))
-bloom$color <- as.list(strsplit(bloom$color, ","))
-bloom$material <- str_replace_all(bloom$material, "(c\\(|\\)|\"| )", "")
-bloom$material <- as.list(strsplit(bloom$material, ","))
+ck$color <- str_to_lower(str_replace_all(ck$color, "(c\\(|\\)|\"| )", ""))
+ck$color <- as.list(strsplit(ck$color, ","))
+ck$material <- str_replace_all(ck$material, "(c\\(|\\)|\"| )", "")
+ck$material <- as.list(strsplit(ck$material, ","))
 
 # Remove observations without 'name'
-bloom$name <- str_trim(bloom$name)
-bloom <- filter(bloom, !is.na(name))
+ck$name <- str_trim(ck$name)
+ck <- filter(ck, !is.na(name))
 
 # Clean up 'type' column and add missing values
-bloom$type <- str_replace(bloom$type, "c\\(\"briefs\", \"boxer briefs\"\\)", "boxer briefs")
-bloom$type[is.na(bloom$type) & str_detect(str_to_lower(bloom$name), "trunk")] <- "trunks"
-bloom$type[is.na(bloom$type) & str_detect(str_to_lower(bloom$name), "boxer brief")] <- "boxer briefs"
-bloom$type[is.na(bloom$type) & str_detect(str_to_lower(bloom$name), "brief")] <- "briefs"
-bloom <- filter(bloom, !is.na(type))
+ck$type <- "boxer briefs"
 
 # Remove observations without 'price'
-bloom <- filter(bloom, !is.na(price.original))
+ck <- filter(ck, !is.na(price.original))
+
+## ---------------- SAVE AND REPEAT FOR TWO OTHER TYPES ----------------
+
+ck_main <- ck
+
+# Handle CK Briefs
+ck <- read.csv2("https://raw.githubusercontent.com/parastyle/DATA-607---Final-Project-/master/Data/ckbriefsResults.csv")
+ck <- ck %>%
+  gather(product, value, 2:ncol(ck)) %>%
+  spread(X, value)
+colnames(ck) <- c("product.id", "type", "name", "color", "material", "price.original", "price.sale", "reviews", "source")
+ck$product.id <- as.numeric(str_replace(ck$product.id, "V", ""))
+ck$product.id <- ck$product.id + 2000
+ck$reviews <- str_trim(ck$reviews)
+ck$color <- str_to_lower(str_replace_all(ck$color, "(c\\(|\\)|\"| )", ""))
+ck$color <- as.list(strsplit(ck$color, ","))
+ck$material <- str_replace_all(ck$material, "(c\\(|\\)|\"| )", "")
+ck$material <- as.list(strsplit(ck$material, ","))
+ck$name <- str_trim(ck$name)
+ck <- filter(ck, !is.na(name))
+ck$type <- "briefs"
+ck <- filter(ck, !is.na(price.original))
+
+ck_main <- rbind(ck_main, ck)
+
+# Handle CK Trunks
+ck <- read.csv2("https://raw.githubusercontent.com/parastyle/DATA-607---Final-Project-/master/Data/ckTrunksResults.csv")
+ck <- ck %>%
+  gather(product, value, 2:ncol(ck)) %>%
+  spread(X, value)
+colnames(ck) <- c("product.id", "type", "name", "color", "material", "price.original", "price.sale", "reviews", "source")
+ck$product.id <- as.numeric(str_replace(ck$product.id, "V", ""))
+ck$product.id <- ck$product.id + 3000
+ck$reviews <- str_trim(ck$reviews)
+ck$color <- str_to_lower(str_replace_all(ck$color, "(c\\(|\\)|\"| )", ""))
+ck$color <- as.list(strsplit(ck$color, ","))
+ck$material <- str_replace_all(ck$material, "(c\\(|\\)|\"| )", "")
+ck$material <- as.list(strsplit(ck$material, ","))
+ck$name <- str_trim(ck$name)
+ck <- filter(ck, !is.na(name))
+ck$type <- "trunks"
+ck <- filter(ck, !is.na(price.original))
+
+ck_main <- rbind(ck_main, ck)
 
 ## ---------------- CONVERT TO TIDY FORMAT ----------------
 
-bloom_tidy <- bloom %>% 
+ck_tidy <- ck_main %>% 
   gather(key, value, 2:9) %>% 
   unnest(value) %>% 
   filter(!is.na(value))
 
 ## ---------------- SAVE FOR FUTURE ANALYSIS  ----------------
 
-write.csv(bloom_tidy, "bloomingdales-tidy.csv", row.names = FALSE)
-saveRDS(bloom, "bloomingdales.rds")
+write.csv(ck_tidy, "ck-tidy.csv", row.names = FALSE)
+saveRDS(ck_main, "ck.rds")
